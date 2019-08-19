025644e827b3c25839f9096531768a9cc2a438cf
dmunter2 <devon.munter@gmail.com>
2019-08-18 20:29:28 -0700

    syling for episode card

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,12 +1,36 @@
 import React from "react";
+import styled from 'styled-components';
+
+
+const DIV1234 = styled.div`
+border: 2px solid lightgrey;
+border-radius: 10px;
+width: 40%;
+margin 5%;
+padding: 1% 0% 0 3%;
+`
+const NAME2 = styled.h2`
+margin: 0;
+`
+
+const AIR_DATE = styled.h3`
+margin: 0;
+margin-top: 2%;
+color: grey;
+`
+const EPISODE = styled.div`
+display: flex;
+`
+
 export default function LocationCard(props) {
 
+
     return (
-        <div className='location-list'>
-            <div>{props.name}</div>
-            <div>{props.air_date}</div>
-            <div>{props.episode}</div>
-        </div>
+        <DIV1234 className='location-list '>
+            <NAME2>{props.name}</NAME2>
+            <AIR_DATE>{props.air_date}</AIR_DATE>
+            <EPISODE>{props.episode}</EPISODE>
+        </DIV1234>
     )
 }
 
